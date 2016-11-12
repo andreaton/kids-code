2461e6d391597efd9f1d5bfecfd03fb59a95dcce
andreaton <andrea.toncelli@gmail.com>
2016-11-12 17:18:36 -0500

    Some code checked in - idea
    Some code checked in - idea

---
+++ ideas/Input.py
@@ -0,0 +1,49 @@
+import os
+
+#print ('I want to find the area of a square. Give me the length of one of the sides.')
+#side=input('Enter length of the side...')
+#print ( 'The area is...',int(side)*int(side))
+
+print ('I want to find the area of a triangle. Give me the length of the base and height.')
+base=input('Enter length of the base...')
+height=input('Enter length of the height...')
+print ( 'The area is...',int(base)*int(height)/int(2))
+
+#print("What is your favorite TV show?")
+#silvia=input("enter your favorite TV show..")
+#print("I like ",silvia," as well")
+
+#print("What is your favorite food?")
+#food=input("enter your favorite food..")
+#print("I don't like",food, )
+
+#print ('Hello! What is your name?')
+#jack= input ('Enter your name ')
+#print ('Hello',jack,'!! Have a nice day!!'  )
+
+
+#print("I want to solve a linear equation for you!!! You have an equation like this ax+b=0")
+#a= input('Enter the value of the letter a: ')
+#b= input('Enter the value of the letter b:')
+
+#print ("In that case x=-b/a which is: ",int(-1) * int(b)/int(a))
+
+#print ('The product of the 2 numbers is... ' ,int(number1)*int(number2))
+#print ('The sum of the 2 numbers is... ' ,int(number1)+int(number2))
+#print ('The diff of the 2 numbers is... ' ,int(number1)-int(number2))
+#print ('The quotient of the 2 numbers is... ' ,int(number1)/int(number2))
+
+
+#person = input('Enter your name: ')
+#print('Hello Babadoo...', person)
+
+#lastname = input('Enter last name')
+#print ('Your name is', person, ' and your last name is..', lastname)
+
+
+#birthday= input ('Enter birthday')
+#print ('Your birthday is...' , birthday)
+
+
+
+exit(0)
